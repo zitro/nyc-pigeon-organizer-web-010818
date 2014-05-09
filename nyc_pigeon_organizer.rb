557a9a33cc66a8c4c36196fc6cc570627a5067b1
Blake Johnson <blake41@gmail.com>
2014-05-09 16:14:56 -0400

    added another solution

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -40,6 +40,40 @@
 # end
 
 # THIS WORKS BUT IS STUPID COMPLEX
+# blakes solution with comments
+# def nyc_pigeon_organizer(data)
+
+#   new_pigeon = {}
+#   pigeon_data.each_pair do |key, value|
+#     # keys color, gender, lives
+#     # value => {
+#     #   :purple => ["Theo", "Peter Jr.", "Lucky"],
+#     #   :grey => ["Theo", "Peter Jr.", "Ms .K"],
+#     #   :white => ["Queenie", "Andrew", "Ms .K", "Alex"],
+#     #   :brown => ["Queenie", "Alex"]
+#     # }
+#     value.each_pair do |attribute, name_array|
+#       # attribute => purple, grey
+#       # name array => ["Theo", "Peter Jr.", "Lucky"]
+#       name_array.each do |name|
+#         # "theo"
+#         if new_pigeon[name].nil?
+#           new_pigeon[name] = {}
+#         end
+#         # new_pigeon = {"theo" => {:color => []}}
+#         if new_pigeon[name][:color].nil?
+#           new_pigeon[name][:color] = []
+#         end
+#         if key == :color
+#           new_pigeon[name][key] << attribute
+#         else
+#           # name => "theo", "key" => gender or lives, "attribute" => "male", female, subway
+#           new_pigeon[name][key] = attribute
+#         end
+#       end
+#     end
+#   end
+# end
 
 def nyc_pigeon_organizer(data)
   organized = {}
