0474b8128ce3e999424d3e968d96ac01ab338992
Bryan Ortiz <bryan@bryanortiz.com>
2018-01-08 00:28:20 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -5,11 +5,11 @@ def nyc_pigeon_organizer(data)
     data.each do |trait, options|
       options.each do |option, pigeons|
         pigeons.each do |pigeon|
-          redonePigeonHash[pigeon] *= {}
-          redonePigeonHash[pigeon][trait] *= []
+          redonePigeonHash[pigeon] ||= {}
+          redonePigeonHash[pigeon][trait] ||= []
           oredonePigeonHash[pigeon][trait] << option.to_s
         end
       end
     end
-    redonePigeonHash
+    organized
   end
